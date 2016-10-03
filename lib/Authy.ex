aa545074889f24d312e507307145b704853446fb
Vidhun <vidhunv1@gmail.com>
2016-10-04 01:43:29 +0530

    Authy: verify otp

--- lib/Authy.ex
+++ lib/Authy.ex
@@ -37,7 +37,26 @@ defmodule Authy do
   				# Logger.warn("Authy: error making verification request." <> reason)
   				{:error, reason}
   		end
+  	else
+  		Logger.warn "Authy API key not configured. Edit 'authy_api_token' in config.exs"
+  	end
+  end
 
+  def verify_otp(country_code, phone, verification_code) do
+  	Authy.start
+
+  	if is_binary(api_key) do
+  		headers = %{"X-Authy-API-Key" => api_key}
+
+  		case Authy.get("/check?phone_number="<>phone<>"&country_code="<>country_code<>"&verification_code="<>verification_code, headers) do
+  			{:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+  				{:ok, body}
+  			{:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
+  				{:error, body[:message]}
+  			{:error, %HTTPoison.Error{reason: reason}} ->
+  				# Logger.warn("Authy: error making verification request." <> reason)
+  				{:error, reason}
+  		end
   	end
   end
 end
