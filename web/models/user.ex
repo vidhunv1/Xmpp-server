ca14dbc23bdd1d3062d9047665f791715319131b
Vidhun <vidhunv1@gmail.com>
2016-10-12 18:11:26 +0530

    user migration for Authy fields

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,6 +8,8 @@ defmodule Spotlight.User do
     field :phone_formatted, :string, size: 25
     field :verification_uuid, :string, size: 50
     field :verification_status, :boolean, default: false
+    field :is_cellphone, :boolean, default: false
+    field :mobile_carrier, :string, size: 100
 
     timestamps()
   end
@@ -16,8 +18,7 @@ defmodule Spotlight.User do
   Builds a changeset based on the `struct` and `params`.
   """
   @required_fields ~w(phone country_code phone_formatted)
-  @optional_fields ~w()
-
+  @optional_fields ~w(verification_uuid is_cellphone mobile_carrier)
   def create_changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
@@ -25,8 +26,15 @@ defmodule Spotlight.User do
   end
 
   @required_fields ~w()
-  @optional_fields ~w(name verification_uuid verification_status)
+  @optional_fields ~w(name)
   def update_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
+  @required_fields ~w(verification_status)
+  @optional_fields ~w()
+  def verify_changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
   end
