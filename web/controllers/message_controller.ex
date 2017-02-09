e1182a2d47cb2ad73aa7a7b1bdc86e4b83b985dc
Vidhun <vidhunv1@gmail.com>
2017-02-10 02:17:47 +0530

    change bot api to json

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -6,13 +6,13 @@ defmodule Spotlight.MessageController do
 
   plug Guardian.Plug.EnsureAuthenticated, [handler: Spotlight.GuardianErrorHandler] when action in [:update]
 
-  def send_message(conn, %{"to_id" => to, "message" => message}) do
+  def send_message(conn, %{"recipient" => to, "message" => message}) do
   	user = Guardian.Plug.current_resource(conn)
-  	from_username = user.username
-  	to_username = Repo.get_by(User, [user_id: to]).username
 
     if(!is_nil(user)) do
-      MessageRouter.send_message(from_username, to_username, message)
+      from_username = user.username
+      to_username = Repo.get_by(User, [user_id: to]).username
+      MessageRouter.send_message(from_username, to_username, Poison.encode!(message))
       send_resp(conn, :ok, "")
     else
       send_resp(conn, 401, "Could not find user.")
