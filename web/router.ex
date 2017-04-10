bd8db7c62542f38eff8efe559d50fcc9b752040f
Vidhun <vidhunv1@gmail.com>
2017-04-10 13:05:54 +0530

    app version get fix

--- web/router.ex
+++ web/router.ex
@@ -30,7 +30,7 @@
     post "/message", MessageController, :send_message
 
     get "/app/init", AppController, :init
-    get "/app/version", AppController, :app_version
+    get "/app/version/:platform", AppController, :app_version
 
     post "/payment", PaymentsController, :create
     get "/payment/:transaction_id", PaymentsController, :get
