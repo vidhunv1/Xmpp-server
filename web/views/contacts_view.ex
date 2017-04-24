b16565443b4780a2b935db5ea88d35d219f6fb98
Vidhun <vidhunv1@gmail.com>
2017-04-25 00:38:54 +0530

    update contact view

--- web/views/contacts_view.ex
+++ web/views/contacts_view.ex
@@ -9,10 +9,16 @@ defmodule Spotlight.ContactsView do
 
   def render("user.json", %{contacts: contact}) do
     user = contact.contact
+    user_type = case user.username do
+      "o_"<>_ -> "official"
+      _ -> "regular"
+    end
+
    %{name: user.name,
      is_registered: user.is_registered,
      profile_dp: SpotlightApi.ImageUploader.url({user.profile_dp, user}),
      username: user.username,
-     user_id: user.user_id}
+     user_id: user.user_id,
+     user_type: user_type}
  end
 end
