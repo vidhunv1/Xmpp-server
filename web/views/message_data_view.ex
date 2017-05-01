f36876cb768c1409674e38f963095e3c9d26fe84
Vidhun <vidhunv1@gmail.com>
2017-05-01 16:07:52 +0530

    data url change response

--- web/views/message_data_view.ex
+++ web/views/message_data_view.ex
@@ -2,7 +2,7 @@ defmodule Spotlight.MessageDataView do
   use Spotlight.Web, :view
   def render("message_image.json", %{user: user, image: image}) do
     %{
-      image_url: SpotlightApi.ImageUploader.url({image, user})
+      data_url: SpotlightApi.ImageUploader.url({image, user})
     }
   end
 end
