fcccef374ec90351e362bf40fcc4172fff3573a8
Vidhun <vidhunv1@gmail.com>
2017-05-01 19:38:21 +0530

    fix

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -21,7 +21,7 @@ defmodule Spotlight.MessageController do
     changeset = user |> Ecto.build_assoc(:message_data) |> Spotlight.MessageData.create_data(%{"data" => image_data, "data_type" => "image"})
     case Repo.insert(changeset) do
       {:ok, mi} ->
-        render(conn, "message_image.json", %{user: user, image: mi.data})
+        render(Spotlight.MessageDataView, "message_image.json", %{user: user, image: mi.data})
       {:error, changeset} ->
         Logger.debug inspect(changeset)
         conn
