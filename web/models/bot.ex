b9822a81784020f3fb0326b91cd803dc2480e41b
Vidhun <vidhunv1@gmail.com>
2017-08-02 19:43:37 +0530

    stars to bots

--- web/models/bot.ex
+++ web/models/bot.ex
@@ -9,13 +9,14 @@ defmodule Spotlight.Bot do
 		field :category, :string, size: 100
     field :cover_picture, :string, size: 100
     field :description, :string, size: 250
+    field :stars, :integer, default: 0
 
   	belongs_to :user, Spotlight.User
     timestamps
   end
 
   @required_fields ~w(post_url)
-  @optional_fields ~w(persistent_menu should_app_init_hook category cover_picture description)
+  @optional_fields ~w(persistent_menu should_app_init_hook category cover_picture description stars)
   def changeset(model, params \\  %{}) do
     model
     |> cast(params, @required_fields, @optional_fields)
